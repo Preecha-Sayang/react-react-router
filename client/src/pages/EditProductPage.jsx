04216b6caa1aa88114ea0915ca0df54a4660cf25
Preecha <preecha.sayang@gmail.com>
2025-08-19 16:04:02 +0700

    add/feature:effect-add

--- client/src/pages/EditProductPage.jsx
+++ client/src/pages/EditProductPage.jsx
@@ -1,11 +1,20 @@
 import EditProductForm from "../components/EditProductForm";
+import { useNavigate } from "react-router-dom";
+
+
+
+
 
 function EditProductPage() {
+const navigate= useNavigate()
+  function backtohome(){
+    navigate("/")
+  }
   return (
     <div>
       <h1>Edit Product Page</h1>
       <EditProductForm />
-      <button>Back to Home</button>
+      <button onClick={backtohome}>Back to Home</button>
     </div>
   );
 }
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,12 +1,41 @@
+import { BrowserRouter, Routes, Route, Link, useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function ViewProductPage() {
+  const {productID}  = useParams();
+  const [data, isdata] =useState([])
+  
+
+
+  const navigate= useNavigate()
+  function backtohome(){
+    navigate("/")
+  }
+  
+  async function getdata() {
+    
+    const newdata= await axios.get(`http://localhost:4001/products/${productID}`)
+    isdata(newdata.data.data)
+  }
+
+useEffect(() => {
+  getdata()
+  }, []);
+
+
+console.log(data)
   return (
     <div>
       <h1>View Product Page</h1>
       <div className="view-product-container">
-        <h2>Product Title</h2>
-        <p>Content</p>
+        <h2>Product Title: {data.name}</h2>
+        <p>price: {data.price}</p>
+        <img src={data.image} alt="img-view" />
+
       </div>
-      <button>Back to Home</button>
+
+      <button onClick={backtohome}>Back to Home</button>
     </div>
   );
 }
